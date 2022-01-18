fa78307e0f7b5753bc514ac4df99e083fa629251
Veselin <vesselin60@gmail.com>
2022-01-18 17:06:04 +0200

    initial commit

---
+++ src/pages/Cart.js
@@ -0,0 +1,9 @@
+function Cart() {
+    return (
+        <main className="cart-page">
+            <h1>Check out</h1>
+        </main>
+    );
+}
+
+export default Cart;
---
+++ src/pages/Photos.js
@@ -0,0 +1,9 @@
+function Photos() {
+    return (
+        <main className="photos">
+            <h1>Images go here</h1>
+        </main>
+    );
+}
+
+export default Photos;
