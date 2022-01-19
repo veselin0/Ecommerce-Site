76a8a821c04e99961744f481ed8f13f394e39f63
Veselin <vesselin60@gmail.com>
2022-01-19 17:33:36 +0200

    add image to cart

--- src/Context.js
+++ src/Context.js
@@ -4,6 +4,7 @@ const Context = createContext();
 
 const ContextProvider = ({ children }) => {
     const [allPhotos, setAllPhotos] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
     const url =
         "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
@@ -27,8 +28,14 @@ const ContextProvider = ({ children }) => {
         setAllPhotos(updatedArr);
     };
 
+    const addToCart = (newItem) => {
+        setCartItems(prevItems => [...prevItems, newItem]);
+    }
+
+    console.log(cartItems);
+
     return (
-        <Context.Provider value={{ allPhotos, toggleFavorite }}>
+        <Context.Provider value={{ allPhotos, toggleFavorite, addToCart }}>
             {children}
         </Context.Provider>
     );
--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,7 @@ import { Context } from "../Context";
 
 const Image = ({ className, img }) => {
     const [hovered, setHovered] = useState(false);
-    const { toggleFavorite } = useContext(Context);
+    const { toggleFavorite, addToCart } = useContext(Context);
 
     const heartIcon = () => {
         if (img.isFavorite) {
@@ -24,7 +24,12 @@ const Image = ({ className, img }) => {
         }
     };
 
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+    const cartIcon = hovered && (
+        <i
+            className="ri-add-circle-line cart"
+            onClick={() => {addToCart(img);}}
+        ></i>
+    );
 
     return (
         <div
@@ -44,8 +49,8 @@ Image.propTypes = {
     img: PropTypes.shape({
         id: PropTypes.string.isRequired,
         url: PropTypes.string.isRequired,
-        isFavorite: PropTypes.bool
-    })
-}
+        isFavorite: PropTypes.bool,
+    }),
+};
 
 export default Image;
