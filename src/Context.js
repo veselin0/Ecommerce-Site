9765549d1e185400bc80cdbd0b0c372a1f3f6afe
Veselin <vesselin60@gmail.com>
2022-01-19 08:30:15 +0200

    setup context state

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const Context = createContext();
 
 const ContextProvider = ({children}) => {
+    const [allPhotos, setAllPhotos] = useState([]);
+
     return (
-        <Context.Provider value="">
+        <Context.Provider value={{allPhotos}}>
             {children}
         </Context.Provider>
     );
