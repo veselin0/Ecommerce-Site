fa980316e767c3403ed03b9cf3054f0accb7961f
Veselin <vesselin60@gmail.com>
2022-01-20 09:22:49 +0200

    minor ux improvements

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,16 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../Context";
 
-const CartItem = ({item}) => {
-    const {removeFromCart} = useContext(Context);
+const CartItem = ({ item }) => {
+    const [hovered, setHovered] = useState(false);
+    const { removeFromCart } = useContext(Context);
+
+    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line";
 
     return (
         <div className="cart-item">
-            <i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)}></i>
-            <img src={item.url} width="130px" alt=""/>
+            <i
+                className={iconClassName}
+                onClick={() => removeFromCart(item.id)}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            ></i>
+            <img src={item.url} width="130px" alt="" />
             <p>$5.99</p>
         </div>
     );
-}
+};
 
 export default CartItem;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -23,16 +23,20 @@ function Cart() {
             setButtonText("Place Order");
             emptyCart();
         }, 3000);
-    }
+    };
 
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
             <p className="total-cost">Total: {totalCostDisplay} </p>
-            <div className="order-button">
-                <button onClick={placeOrder}>{buttonText}</button>
-            </div>
+            {cartItems.length > 0 ? (
+                <div className="order-button">
+                    <button onClick={placeOrder}>{buttonText}</button>
+                </div>
+            ) : (
+                <p>You have no items in your cart.</p>
+            )}
         </main>
     );
 }
