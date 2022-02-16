1fab230025b25dc1c1520836de579f153c114664
Veselin <vesselin60@gmail.com>
2022-02-16 11:30:29 +0200

    readfy to deploy to github pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import {Switch, Route} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Photos from "./pages/Photos";
 import Cart from "./pages/Cart";
 import Header from "./components/Header";
 
-
-function App() {    
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path="/"><Photos /></Route>
-                <Route path="/cart"><Cart /></Route>
-            </Switch>
-        </div>
-    )
+function App() {
+	return (
+		<div>
+			<Header />
+			<Switch>
+				<Route exact path="/">
+					<Photos />
+				</Route>
+				<Route path="/cart">
+					<Cart />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
-export default App
+export default App;
